430b741edaef02c290211cfff21c0a199e2c45af
Miguel Saymon <miguel.saymon05@gmail.com>
2026-06-14 22:39:20 +0000

    Adiciona metade do nivel nas pericias

--- scripts/calculations/pericias.js
+++ scripts/calculations/pericias.js
@@ -7,25 +7,33 @@ export function calcularPericias(system) {
   const pericias = system.pericias ?? {};
   const atributos = system.atributos ?? {};
 
+  const nivel = Number(system.info?.nivel ?? 0);
+  const metadeNivel = Math.floor(nivel / 2);
+
   for (const pericia of Object.values(pericias)) {
     const atributoChave = pericia.atributo;
     const atributo = atributos[atributoChave];
 
     const modificador = Number(atributo?.mod) || 0;
 
-const treino =
-  pericia.treinado
-    ? obterBonusTreino(system)
-    : 0;
+    const treino =
+      pericia.treinado
+        ? obterBonusTreino(system)
+        : 0;
 
-const maestria =
-  pericia.maestria
-    ? obterBonusMaestria(system)
-    : 0;
+    const maestria =
+      pericia.maestria
+        ? obterBonusMaestria(system)
+        : 0;
 
-    const outros = Number(pericia.outros) || 0;
+    const outros = Number(pericia.outro) || 0;
 
-    pericia.total = modificador + treino + maestria + outros;
+    pericia.total =
+      modificador +
+      metadeNivel +
+      treino +
+      maestria +
+      outros;
 
     pericia.totalFormatado =
       pericia.total >= 0
@@ -35,4 +43,3 @@ const maestria =
 
   return pericias;
 }
-
