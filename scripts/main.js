933339b5f0d297ecf53b4c5f459491b972c1e33b
Miguel Saymon <miguel.saymon05@gmail.com>
2026-08-09 15:11:37 +0000

    chore: remove importadores temporarios dos compendiums

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,3 @@
-import { seedFMTalentos } from "./import/fm-talentos-seeder.js";
-import { seedFMLendariasMelhorias } from "./import/fm-lendarias-melhorias-seeder.js";
-import { seedFMHabilidadesEspecializacao } from "./import/fm-habilidades-especializacao-seeder.js";
-import { seedFMEquipamentos } from "./import/fm-equipamentos-seeder.js";
-import { seedFMFeiticos } from "./import/fm-feiticos-seeder.js";
-import { seedFMAptidoes } from "./import/fm-aptidoes-seeder.js";
-import { seedFMSamples } from "./import/fm-sample-seeder.js";
-import { FMConverter } from "./import/fm-converter.js";
 import { JKCharacterSheet } from "./sheets/character-sheet.js";
 import { JKInvocacaoSheet } from "./sheets/invocacao-sheet.js";
 import { JKItemSheet } from "./sheets/item-sheet.js";
@@ -15,14 +7,6 @@ Hooks.once("init", async function () {
   console.log("Jiujitsu Kaisen | Sistema inicializado com sucesso.");
 
   game.jiujitsuKaisen = game.jiujitsuKaisen ?? {};
-  game.jiujitsuKaisen.fmConverter = FMConverter;
-  game.jiujitsuKaisen.seedFMSamples = seedFMSamples;
-  game.jiujitsuKaisen.seedFMAptidoes = seedFMAptidoes;
-  game.jiujitsuKaisen.seedFMFeiticos = seedFMFeiticos;
-  game.jiujitsuKaisen.seedFMEquipamentos = seedFMEquipamentos;
-  game.jiujitsuKaisen.seedFMHabilidadesEspecializacao = seedFMHabilidadesEspecializacao;
-  game.jiujitsuKaisen.seedFMLendariasMelhorias = seedFMLendariasMelhorias;
-  game.jiujitsuKaisen.seedFMTalentos = seedFMTalentos;
 
   await loadTemplates([
     "systems/jiujitsu-kaisen/templates/partials/header.hbs",
@@ -96,30 +80,5 @@ Hooks.on("createItem", (item) => {
 
 Hooks.on("deleteItem", (item) => {
   renderizarActorPaiDoItem(item);
-});
-
-
-
-Hooks.once("ready", async function () {
-  if (!game.user?.isGM) return;
-
-  const sincronizar = async (nome, callback) => {
-    try {
-      return await callback();
-    } catch (error) {
-      console.error(`Jiujitsu Kaisen | Falha ao sincronizar ${nome}.`, error);
-      ui.notifications?.error(`Jiujitsu Kaisen | Falha ao sincronizar ${nome}. Consulte o console.`);
-      return null;
-    }
-  };
-
-  // Talentos primeiro para que o catálogo final não dependa do sucesso dos demais seeders.
-  await sincronizar("Talentos", () => seedFMTalentos({ notify: true }));
-  await sincronizar("Aptidões Amaldiçoadas", () => seedFMAptidoes({ notify: true }));
-  await sincronizar("Feitiços", () => seedFMFeiticos({ notify: true }));
-  await sincronizar("Equipamentos", () => seedFMEquipamentos({ notify: true }));
-  await sincronizar("Habilidades de Especialização", () => seedFMHabilidadesEspecializacao({ notify: true }));
-  await sincronizar("Habilidades Lendárias e Melhorias Superiores", () => seedFMLendariasMelhorias({ notify: true }));
-  await sincronizar("amostras restantes", () => seedFMSamples({ notify: true }));
 });
 
